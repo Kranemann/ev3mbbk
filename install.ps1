3962fc05fc0fb9c4fca147d3f4637c1cc9222cb2
Jwan Karo <Jwankrho@gmail.com>
2022-04-30 19:11:08 +0200

    Fix Bugs

--- install.ps1
+++ install.ps1
@@ -1,21 +1,19 @@
 # Requires -RunAsAdministrator
 
 function exists {
- 
-
     param (
         [string]$command
     )
-    if (& { $command } 2>&1 -is [System.Management.Automation.ErrorRecord]) { $true } Else { $false }
+    if (& { $command } 2>&1 -is [System.Management.Automation.ErrorRecord]) { $false } Else { $true }
 }
 
 
 if (!(exists "choco -V")) {
     # install chocolatey
     Invoke-WebRequest "https://chocolatey.org/install.ps1" -UseBasicParsing | Invoke-Expression    
-    # install python
 }
 
+# install python
 if (!(exists "python -V")) {
     choco install python -y
 }
@@ -24,6 +22,6 @@ if (!(exists "python -V")) {
 pip install -r requirements.txt
 
 # install vs code
-if (!($env:TERM_PROGRAM -eq 'vscode')) {
+if (!(exists 'code -v')) {
     choco install vscode -y
 }
